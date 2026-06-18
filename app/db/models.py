8d195308a81f4390b85a96b75bb0dd80e9ba6af0
Raktabh <gitadb70@gmail.com>
2026-06-18 23:05:30 +0530

    feat: setup async database foundation

---
+++ app/db/models.py
@@ -0,0 +1,22 @@
+# app/db/models.py
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+
+class Link(Base):
+    __tablename__ = "links"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    original_url: Mapped[str] = mapped_column(
+        String(2048)
+    )
+
+    short_code: Mapped[str] = mapped_column(
+        String(20),
+        unique=True,
+        index=True,
+    )
